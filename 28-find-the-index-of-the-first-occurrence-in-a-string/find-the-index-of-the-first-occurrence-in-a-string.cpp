e258fb3a29c7826b04d566fb1e3de1bca462bfd5
Austin Do <austinnguyendo@gmail.com>
2025-09-26 19:09:04 -0500

    Update find-the-index-of-the-first-occurrence-in-a-string.cpp
    Moved explanation for legibility

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -10,21 +10,7 @@ public:
         since otherwise, the needle wont fit any there is not any point in continuing.
         */
 
-        /*for loop explanation
-as long as j is smaller than needle
-what this does is it checks i + j in the sense that it compares the letters of the needle, 
-the for loop checks if it is equal to the hay stack and if the check fails, 
-If any character does not match, 
-stop checking this starting index i then move to the next possible starting index in haystack
-
-haystack = "sadbutsad"
-needle = "sad"
-i = 0 → we start checking from haystack[0]
-j = 0 → compare haystack[0 + 0] = haystack[0] = 's' with needle[0] = 's' 
-j = 1 → compare haystack[0 + 1] = haystack[1] = 'a' with needle[1] = 'a' 
-j = 2 → compare haystack[0 + 2] = haystack[2] = 'd' with needle[2] = 'd' 
-Since all characters match, we know needle occurs starting at i = 0.
-*/
+        
         for (int i = 0; i <= haystack.size() - needle.size(); i++){
             bool match = true;
             for (int j = 0; j < needle.size(); j++){
@@ -39,3 +25,21 @@ Since all characters match, we know needle occurs starting at i = 0.
     }
 };
 
+/*
+-Updated for legibility
+
+for loop explanation
+as long as j is smaller than needle
+what this does is it checks i + j in the sense that it compares the letters of the needle, 
+the for loop checks if it is equal to the hay stack and if the check fails, 
+If any character does not match, 
+stop checking this starting index i then move to the next possible starting index in haystack
+
+haystack = "sadbutsad"
+needle = "sad"
+i = 0 → we start checking from haystack[0]
+j = 0 → compare haystack[0 + 0] = haystack[0] = 's' with needle[0] = 's' 
+j = 1 → compare haystack[0 + 1] = haystack[1] = 'a' with needle[1] = 'a' 
+j = 2 → compare haystack[0 + 2] = haystack[2] = 'd' with needle[2] = 'd' 
+Since all characters match, we know needle occurs starting at i = 0.
+*/
