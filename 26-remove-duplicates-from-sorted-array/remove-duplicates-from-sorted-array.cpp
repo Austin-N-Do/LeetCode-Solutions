d9d1d923284d560a273a5a2ccaf9a3e49aa80475
Austin Do <austinnguyendo@gmail.com>
2025-12-14 19:45:18 -0600

    Time: 0 ms (100.00%) | Memory: 22.7 MB (49.11%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // check edge case
-        if (nums.empty()) return 0;
-
-        // write pointer
-        int i = 0; 
-
-        // iterates through the array
-        for (int j = 1; j < nums.size(); j++) { 
-            // checks if the starting value is the same as the next one
-            if (nums[i] != nums[j]){
-
-                // if the number isnt the same, move forward in the array
-                i++;
-                // set the next number as the new one to compare
-                nums[i] = nums[j];
+        
+        int k = 1;
+        for (int i = 0; i < nums.size(); i++){
+            if (nums[i]!=nums[k-1]){ //in order to get the first element and check the previous number store to see if the numbers are duplicates
+            nums[k]= nums[i];
+            k++;
             }
         }
-        
-        return i + 1;
+        return k;
     }
 };
