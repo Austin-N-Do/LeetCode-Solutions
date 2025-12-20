60a6e3a6532ce9fd0eb4450cc5d82005851a0730
Austin Do <austinnguyendo@gmail.com>
2025-12-20 17:34:52 -0600

    Time: 4 ms (65.81%) | Memory: 13.2 MB (57.72%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> romanValues = {
+    {'I', 1},
+    {'V', 5},
+    {'X', 10},
+    {'L', 50},
+    {'C', 100},
+    {'D', 500},
+    {'M', 1000},
+        };
+    int total = 0;
+    for (int i = 0; i < s.length(); i++){
+        int current = romanValues[s[i]];
+        
+        if (i < s.length()-1) {
+            int next = romanValues[s[i+1]];
+            if (current < next){
+                total -= current;
+            }
+            else{
+                total += current;
+            }
+        }
+        else
+        total += current;
+    }
+    return total;
+    }
+};
