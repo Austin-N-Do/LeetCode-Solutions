2a08c5a2396fe6dce6f966ecde9ab3e74f50e69c
Austin Do <austinnguyendo@gmail.com>
2026-01-05 06:47:14 -0600

    Time: 43 ms (25.73%) | Memory: 14 MB (80.32%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> map;
 
         for (int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
-
-            // Check if complement exists in map
-            if (map.find(complement) != map.end()) {
-                // Found it! Return both indices
-                return {map[complement], i};
+            int remainder = target - nums[i];
+            for (int j = i+1; j < nums.size(); j++) {
+                if (remainder == nums[j]) {
+                    return {i, j};
+                }
             }
-
-            // Didn't find it, so add current number to map
-            map[nums[i]] = i;
         }
         return {};
     }
